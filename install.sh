190095975ccfa210e31e17164c2ef96d2d69b687
undownding <undownding@gmail.com>
2023-06-29 17:32:34 +0800

    fix dir

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ export NVM_NODEJS_ORG_MIRROR="$2"
 git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 cd  "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-cd "$GITHUB_PATH"
+cd "$GITHUB_WORKSPACE"
 chmod +x "$NVM_DIR/nvm.sh"
 . "$NVM_DIR/nvm.sh" --no-use
 nvm install
